86100bac3210017c82c748bfef1b6f2d18dca4fb
MattParker89 <mdparker89@gmail.com>
2013-09-16 17:28:26 -0400

    add base and data types for template send

---
+++ dataTypes.go
@@ -0,0 +1,72 @@
+package mandrill
+
+type Message struct {
+	Html                    string              `json:"html, omitempty"`
+	Text                    string              `json:"text, omitempty"`
+	Subject                 string              `json:"subject"`
+	FromEmail               string              `json:"from_email"`
+	FromName                string              `json:"from_name"`
+	To                      []To                `json:"to"`
+	Headers                 map[string]string   `json:"headers, omitempty"`
+	Important               bool                `json:"important, omitempty"`
+	TrackOpens              bool                `json:"track_opens, omitempty"`
+	TrackClicks             bool                `json:"track_clicks, omitempty"`
+	AutoText                bool                `json:"auto_text, omitempty"`
+	AutoHtml                bool                `json:"auto_html, omitempty"`
+	InlineCss               bool                `json:"inline_css, omitempty"`
+	UrlStripQS              bool                `json:"url_strip_qs, omitempty"`
+	PreserveRecipients      bool                `json:"preserve_recipients, omitempty"`
+	ViewContentLink         bool                `json:"view_content_link, omitempty"`
+	BCCAddress              string              `json:"bcc_address, omitempty"`
+	TrackingDomain          string              `json:"tracking_domain, omitempty"`
+	SigningDomain           string              `json:"signing_domain, omitempty"`
+	ReturnPathDomain        string              `json:"return_path_domain, omitempty"`
+	Merge                   bool                `json:"merge, omitempty"`
+	GlobalMergeVars         []GlobalVar         `json:"global_merge_vars, omitempty"`
+	MergeVars               []MergeVars         `json:"merge_vars, omitempty"`
+	Tags                    []string            `json:"tags, omitempty"`
+	Subaccount              string              `json:"subaccount, omitempty"`
+	GoogleAnalyticsDomains  []string            `json:"google_analytics_domains, omitempty"`
+	GoogleAnalyticsCampaign []string            `json:"google_analytics_campaign, omitempty"`
+	Metadata                []map[string]string `json:"metadata, omitempty"`
+	RecipientMetadata       []RecipientMetaData `json:"recipient_metadata, omitempty"`
+	Attachments             []Attachment        `json:"attachments, omitempty"`
+	Images                  []Image             `json:"images, omitempty"`
+}
+
+type To struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+type MergeVars struct {
+	Recipient string      `json:"rcpt"`
+	Vars      []GlobalVar `json:"vars"`
+}
+
+type GlobalVar struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type TemplateContent struct {
+	Name    string `json:"name"`
+	Content string `json:"code"`
+}
+
+type Attachment struct {
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type Image struct {
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+type RecipientMetaData struct {
+	Recipient string            `json:"rcpt"`
+	Vars      map[string]string `json:"values"`
+}
---
+++ mandrill.go
@@ -0,0 +1,40 @@
+package mandrill
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+var baseURL string = "https://mandrillapp.com/api/"
+var Version string = "1.0"
+var Format string = "json"
+var APIkey string
+
+type MandrillCall struct {
+	Version  string
+	Format   string
+	APIkey   string
+	Method   string
+	Category string
+	Args     map[string]interface{}
+}
+
+func NewCall() *MandrillCall {
+	return &MandrillCall{
+		Version: Version,
+		Format:  Format,
+		APIkey:  APIkey,
+		Args:    make(map[string]interface{}),
+	}
+}
+
+func (m *MandrillCall) Send() (*http.Response, error) {
+	m.Args["key"] = APIkey
+	args, _ := json.Marshal(m.Args)
+	url := baseURL + "/" + m.Version + "/" + m.Category + "/" + m.Method + "." + m.Format
+	client := &http.Client{}
+	r, _ := http.NewRequest("POST", url, bytes.NewReader(args))
+	resp, err := client.Do(r)
+	return resp, err
+}
